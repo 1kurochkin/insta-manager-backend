5b8bb63986470923f8c9b1c8552db53dc96805f3
Bob Developer <bobdeveloperbob@gmail.com>
2023-01-09 01:13:26 -0600

    made general update insta_manager.service.ts

--- src/services/insta_manager.service.ts
+++ src/services/insta_manager.service.ts
@@ -1,76 +1,87 @@
-import puppeteer from "puppeteer";
-import {ElementHandle} from "puppeteer";
+import {ElementHandle, Page} from "puppeteer";
 import axios from "axios";
 
-class InstaManagerService {
-    domain = 'https://www.instagram.com'
-    cookies: Array<any> = [];
-    appId: string | undefined;
+type Follower = {username: string, profile_pic_url: string, full_name: string}
 
-    constructor(
-        private readonly username: string,
-        private readonly password: string,
-        private readonly puppeteerService: typeof puppeteer,
-        private readonly axiosService: typeof axios
-    ) {
-        this.username = username;
-        this.password = password;
-        this.puppeteerService = puppeteerService;
-        this.axiosService = axiosService;
+export class InstaManagerService {
+    private cookies: string | undefined;
+    private appId: string | undefined;
+
+    constructor(private readonly domain: string) {
+        this.domain = domain;
     }
 
-    async fetch(url: string) {
+    private async fetch(url: string) {
         return axios(
             `${this.domain}/api/v1/${url}`,
-            {headers: {'x-ig-app-id': this.appId, 'cookies': this.cookies}}
+            {
+                headers: {'x-ig-app-id': this.appId, "cookie": this.cookies},
+            }
         )
     }
-    setCookies(cookies: Array<any>) {
-        this.cookies = cookies;
+    private setCookies(cookies: Array<any>) {
+        const cookiesArray = []
+        for (const {name, value} of cookies) {
+            if(name === 'sessionid' || name === 'ds_user_id') {
+                cookiesArray.push(`${name}=${value};`)
+            }
+        }
+        this.cookies = cookiesArray.join(' ')
     }
-    setAppId(appId: string) {
+    private setAppId(appId: string) {
         this.appId = appId;
     }
 
-    async login() {
-        const browser = await this.puppeteerService.launch();
-        const page = await browser.newPage();
-        await page.goto(this.domain);
-
+    async login(loginPage: Page, username: string, password: string) {
         // @ts-ignore
-        const usernameInput = await page.waitForSelector("input[name='username']");
+        const usernameInput = await loginPage.waitForSelector("input[name='username']");
         // @ts-ignore
-        const passwordInput = await page.waitForSelector("input[name='password']");
+        const passwordInput = await loginPage.waitForSelector("input[name='password']");
         if(usernameInput && passwordInput) {
-            await usernameInput.type(this.username);
-            await passwordInput.type(this.password);
-            await page.evaluate(() => {
+            await usernameInput.type(username);
+            await passwordInput.type(password);
+            loginPage.on('request', (req:any) => {
+                const {'x-ig-app-id': appId} = req.headers();
+                if(appId) {
+                    this.setAppId(appId)
+                    loginPage.removeAllListeners('request')
+                }
+            });
+            await loginPage.evaluate(() => {
                 (document.querySelector('button[type=submit]') as unknown as ElementHandle).click();
             });
-
             await new Promise(r => setTimeout(r, 5000));
-            this.setCookies(await page.cookies());
-        } else throw 'There is not inputs for sign in!'
+            this.setCookies(await loginPage.cookies());
+        } else throw 'There are not inputs for sign in!'
     }
     async getUserInfo (nickname: string) {
         const response = await this.fetch(`users/web_profile_info/?username=${nickname}`);
-        console.log(response); // data.user.id // data.user.edge_followed_by.count // data.user.edge_follow.count
+        return response.data.data.user;
     }
-    async getFollowers (userId: string, count: number) {
-        const response = await this.fetch(`friendships/${userId}/followers/?count=${count}&search_surface=follow_list_page`);
-        console.log(response); // data.users.username
+    async getFollowers (userId: string, take:number, page:number) {
+        const response = await this.fetch(`friendships/${userId}/followers/?count=${take}&max_id=${take * page}&search_surface=follow_list_page`);
+        return response.data.users;
     }
-    async getFollowings (userId: string, count: number) {
-        const response = await this.fetch(`friendships/${userId}/following/?count=${count}`);
-        console.log(response); // data.users.username
+    async getFollowings (userId: string, take:number, page:number) {
+        const response = await this.fetch(`friendships/${userId}/following/?count=${take}&max_id=${take * page}`);
+        return response.data.users;
     }
-    async getUnfollowed (followers: Array<{username: string}>, following: Array<{username: string}>) {
+    async getUnfollowed (followers: Array<Follower>, following: Array<Follower>) {
         const setOfFollowers = new Set(followers.map(follower => follower.username));
         return following.reduce((acc, following) => {
             if(!setOfFollowers.has(following.username)) {
-                acc.push(following)
+                const {username, profile_pic_url, full_name} = following;
+                acc.push({username, profile_pic_url, full_name})
             }
             return acc;
         }, [] as Array<any>);
+    }
+    getHowManyTakeFromEachRequest(count:number) {
+        const TAKE = 100;
+       const [firstCountForTakeBy100, lastCountForTake] = `${count / TAKE}`.split('.');
+       return [
+           ...new Array(Number(firstCountForTakeBy100)).fill(TAKE),
+           Number(lastCountForTake)
+       ];
     }
 }
