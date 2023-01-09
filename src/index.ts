8c3c247528757b39faa5d5660c0dc3ceb2b11edf
Bob Developer <bobdeveloperbob@gmail.com>
2023-01-09 01:14:25 -0600

    add logic for server and make common updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import express, {Request, Response, Application} from 'express';
-import bodyParser from "body-parser";
+import express, {Application, Request, Response} from 'express';
 import puppeteer from "puppeteer";
-import axios from "axios";
 import {InstaManagerService} from "./services/insta_manager.service";
-import {startServer} from "./server";
+import {config} from "./configs/config"
 
 
 (async () => {
@@ -11,14 +9,49 @@ import {startServer} from "./server";
         // INITIAL PUPPETEER
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://www.instagram.com');
+        await page.goto(config.instagram.domain);
 
         // INITIAL INSTA-MANAGER-SERVICE
-        const instaManagerService = new InstaManagerService('rapira.88', 'Aawei6ha')
-        await instaManagerService.login(page);
+        const instaManagerService = new InstaManagerService(config.instagram.domain);
+        await instaManagerService.login(
+            page,
+            config.instagram.credentials.username,
+            config.instagram.credentials.password
+        );
 
         // START SERVER
-        await startServer(instaManagerService)
+        const server: Application = express();
+        server.use(express.json())
+
+        server.post("/check", async (req: Request, res: Response) => {
+            const { nickname } = req.body;
+            try {
+                //GET USERINFO FOR TAKE ID AND GET KNOW HOW MANY FOLLOWERS AND FOLLOWINGS USER HAS
+                const {id, edge_followed_by, edge_follow} = await instaManagerService.getUserInfo(nickname);
+
+                //GET ALL FOLLOWERS THAT USER HAS
+                const arrayOfCountOfTakeFollowers = instaManagerService.getHowManyTakeFromEachRequest(edge_followed_by.count);
+                const followers = await Promise.all(
+                    arrayOfCountOfTakeFollowers.map((take, page) => instaManagerService.getFollowers(id, take, page))
+                );
+
+                //GET ALL FOLLOWINGS THAT USER HAS
+                const arrayOfCountOfTakeFollowings = instaManagerService.getHowManyTakeFromEachRequest(edge_follow.count);
+                const followings = await Promise.all(
+                    arrayOfCountOfTakeFollowings.map((take, page) => instaManagerService.getFollowings(id, take, page))
+
+                );
+
+                // MATCH FOLLOWERS AND FOLLOWINGS FOR GET KNOW WHO UNFOLLOWED
+                const unfollowed = await instaManagerService.getUnfollowed(followers.flat(), followings.flat());
+
+                // RETURN UNFOLLOWED USERS
+                res.json(unfollowed);
+            } catch (e) {
+                console.log(e)
+            }
+        });
+        server.listen(config.port, () => console.log(`Server is Running 👉`))
     } catch (e) {
         throw e;
     }
